0e51652905b7b2243bdf5fb129178b44e02bab7f
UB WINGS Lab <40919816+ubwingslab@users.noreply.github.com>
2020-03-24 15:22:37 -0400

    Update ESN_data.py

--- LeTera/ESN_data.py
+++ LeTera/ESN_data.py
@@ -7,7 +7,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # #You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.import flytera_cfg
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import flytera_cfg
 
 import FlyTera
 
